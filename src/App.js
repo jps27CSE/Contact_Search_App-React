05d1277d968fd2b936d2e35d895e81a9b362d05f
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2021-08-08 00:38:51 +0600

    Deploy

--- src/App.js
+++ src/App.js
@@ -33,6 +33,7 @@ class App extends Component {
 
     return (
       <div className="App">
+        <h1>Contact</h1>
         <SearchBox
           placeholder="Search"
           handleChange={this.handleChange}
